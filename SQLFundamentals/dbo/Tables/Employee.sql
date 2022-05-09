afe5f908685d7d7d8c983bc6ca333b2cff047ec7
Jakhongir7 <bakhodirovjakhongir@gmail.com>
2022-05-09 21:05:43 +0500

    Add project files.

---
+++ SQLFundamentals/dbo/Tables/Employee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]           INT            NOT NULL,
+    [AddressId]    INT            NULL,
+    [PersonId]     INT            NULL,
+    [CompanyName]  NVARCHAR (20)  NULL,
+    [Position]     NVARCHAR (30)  NOT NULL,
+    [EmployeeName] NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Address] ([Id]),
+    FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id])
+);
+
