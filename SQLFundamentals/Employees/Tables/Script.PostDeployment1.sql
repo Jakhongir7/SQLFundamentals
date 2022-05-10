f7361243ba37bdceb213db52d5311772556465c0
Jakhongir7 <bakhodirovjakhongir@gmail.com>
2022-05-10 23:15:03 +0500

    Task 1 finished

--- SQLFundamentals/Employees/Tables/Script.PostDeployment1.sql
+++ SQLFundamentals/Employees/Tables/Script.PostDeployment1.sql
@@ -13,3 +13,18 @@ INSERT INTO dbo.Person (Id, FirstName, LastName)
 SELECT 1, 'Jakhongir', 'Bakhodirov'
 WHERE not exists (SELECT 1 FROM  dbo.Person WHERE Id = 1)
 GO
+
+INSERT INTO dbo.Address (Id, Street, City, State, ZipCode)
+SELECT 1, 'Feruza', 'Tashkent', 'Uzbekistan', '100100'
+WHERE not exists (SELECT 1 FROM  dbo.Address WHERE Id = 1)
+GO
+
+INSERT INTO dbo.Employee (Id, AddressId, PersonId, CompanyName, Position, EmployeeName)
+SELECT 1, 1, 1, 'EPAM', 'Software Engineer', 'Jakhongir Bakhodirov'
+WHERE not exists (SELECT 1 FROM  dbo.Employee WHERE Id = 1)
+GO
+
+INSERT INTO dbo.Company (Id, Name, AddressId)
+SELECT 1, 'EPAM', 1
+WHERE not exists (SELECT 1 FROM  dbo.Company WHERE Id = 1)
+GO
